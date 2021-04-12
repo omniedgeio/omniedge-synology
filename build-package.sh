1147cb5ce51dba4152b34af602e2eca17d131264
mtx2d <matrix2d@outlook.com>
2021-04-11 22:10:08 -0400

    add icon and info script

--- build-package.sh
+++ build-package.sh
@@ -48,8 +48,10 @@ make_spk() {
   rm "${spk_tmp_dir}/extractsize_tmp"
 
   # copy scripts and icon
+  cp -a src/PACKAGE_ICON*.PNG ${spk_tmp_dir}
 
   # generate INFO file
+  ./src/INFO.sh "${OMNIEDGE_VERSION}" ${ARCH} ${pkg_size} >"${spk_tmp_dir}"/INFO
 
   tar -cf "${spk_dest_dir}/${spk_filename}" -C "${spk_tmp_dir}" $(ls ${spk_tmp_dir})
 }
